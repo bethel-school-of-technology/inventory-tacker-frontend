69d7477e53a0e68bd03cf0602af60e6207da97ad
Benjamin Kennedy <BJKennedy95@gmail.com>
2019-10-24 21:36:53 -0500

    Updated frontend

--- react-frontend-inventory/src/App.js
+++ react-frontend-inventory/src/App.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Mowers from './components/Mowers';
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Mowers uri="http://localhost:3001/staticMowers" />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Home />
+  </div>
+);
 
 export default App;
--- react-frontend-inventory/src/components/Mowers.js
+++ react-frontend-inventory/src/components/Mowers.js
@@ -1,39 +1,65 @@
-import React from 'react';
+// import React from "react";
+// import axios from "axios";
+// import Modal from "react-modal";
 
-import axios from 'axios';
+// //check out Material UI for CSS
 
-class Mowers extends React.Component {
-  state = {
-    mowerData: []
-  };
+// class Mowers extends React.Component {
+//   state = {
+//     mowerData: [],
+//     modalIsOpen: false
+//   };
 
-  fetchMowerData = () => {
-    var encodedURI = window.encodeURI(this.props.uri);
-    return axios.get(encodedURI).then(response => {
-      this.setState(() => {
-        return {
-          mowerData: response.data
-        };
-      });
-    });
-  };
+//   openModal = this.openModal.bind(this);
+//   closeModal =this.closeModal.bind(this);
 
-  componentDidMount() {
-    this.fetchMowerData();
-  }
+//   openModal() {
+//     this.setState({ modalIsOpen: true });
+//   }
 
-  render() {
-    console.log(this.state.mowerData);
-    if (this.state.mowerData.length === 0) {
-      return <div>Failed to fetch data from server</div>;
-    }
-    const mowers = this.state.mowerData.map(mower => (
-      <div key={mower.name}>
-        <em>{mower.name}</em>: {mower.type}
-      </div>
-    ));
-    return <div>{mowers}</div>;
-  }
-}
+//   closeModal() {
+//     this.setState({ modalIsOpen: false });
+//   }
 
-export default Mowers;
+//   fetchMowerData = () => {
+//     var encodedURI = window.encodeURI(this.props.uri);
+//     return axios.get(encodedURI).then(response => {
+//       this.setState(() => {
+//         return {
+//           mowerData: response.data
+//         };
+//       });
+//     });
+//   };
+
+//   componentDidMount() {
+//     this.fetchMowerData();
+//   }
+
+//   render() {
+//     console.log(this.state.mowerData);
+//     if (this.state.mowerData.length === 0) {
+//       return <div>Failed to fetch data from server</div>;
+//     }
+//     const mowers = this.state.mowerData.map(mower => (
+//       <div key={mower.name}>
+//         <em>{mower.name}</em>
+//       </div>
+//     ));
+//     const mowerType = this.state.mowerData.map(mower => (
+//       <div key={mower.type}>
+//         <em>{mower.type}</em>
+//       </div>
+//     ));
+//     return (
+//       <div>
+//         <button onClick={this.openModal}>{mowers.length > 0}</button>
+//         <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal}>
+//           {mowerType}
+//         </Modal>
+//       </div>
+//     );
+//   }
+// }
+
+// // export default Mowers;
