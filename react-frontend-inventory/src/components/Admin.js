243637ecdb77718a0d86b46d50a217f8198226e1
Kevin Frohlich <kevinfrohlich@gmail.com>
2019-10-31 06:50:41 -0700

    Fixed a few pages with responsiveness, + images.

--- react-frontend-inventory/src/components/Admin.js
+++ react-frontend-inventory/src/components/Admin.js
@@ -2,9 +2,14 @@ import React from 'react';
 import '../App.css';
 
 const Admin = () => (
-	<div>
-		<h1>Administration Page</h1>
-		<h4>List of Employees:</h4>
+	<div class="grid">
+		<div class="col-2-3">
+			<h2>Administration Page</h2>
+			<h4>List of Employees:</h4>
+		</div>
+		<div class="col-1-3">
+			<img src={require('../images/admin.png')} width="auto" alt="admin" />
+		</div>
 		<hr />
 	</div>
 );
