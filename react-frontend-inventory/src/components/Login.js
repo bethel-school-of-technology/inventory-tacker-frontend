b41ae5859ff778382ccaaf85b05805b62846ad8d
Unknown <BJKennedy95@gmail.com>
2019-11-06 17:49:18 -0600

    App is running
    Installed react-router-dom so you'll need to do an npm install when you open it.

--- react-frontend-inventory/src/components/Login.js
+++ react-frontend-inventory/src/components/Login.js
@@ -1,93 +1,99 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import '../App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
 //import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 //import { HashRouter as Router, NavLink, Link } from "react-router-dom";
 
 class Login extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			Username: '',
-			Password: ''
-		};
-		this.onChangeUsername = this.onChangeUsername.bind(this);
-		this.onChangePassword = this.onChangePassword.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
+    this.state = {
+      Username: "",
+      Password: ""
+    };
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-	onChangeUsername(event) {
-		this.setState({
-			Username: event.target.value
-		});
-	}
+  onChangeUsername(event) {
+    this.setState({
+      Username: event.target.value
+    });
+  }
 
-	onChangePassword(event) {
-		this.setState({
-			Password: event.target.value
-		});
-	}
-	onSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			await (this.state.Username, this.state.Password);
-			alert('login successfull!');
-			this.props.history.push('/login');
-		} catch (event) {
-			alert(event.message);
-		}
-		const { Username, Password } = this.state;
-		const apiUrl = 'http://localhost:3001/users/login';
-		return axios
-			.post(apiUrl, {
-				Username,
-				Password
-			})
-			.then((res) => {
-				window.localStorage.setItem('jwt', res.token);
-				if (res.token === window.localStorage.token) {
-					this.props.history.push('/profile');
-				}
-			});
-	};
-	onChange(key, event) {
-		this.setState({
-			[key]: event.target.value
-		});
-	}
-	render() {
-		const { Username, Password } = this.state;
-		return (
-			<React.Fragment>
-				<form id="login" name="login" method="POST" onSubmit={this.onSubmit}>
-					<div>
-						<h2>Please Login</h2>
-						<label>Username:</label>
-						<input type="text" name="username" value={Username} onChange={this.onChangeUsername} required />
-					</div>
-					<div>
-						<label>Password:</label>
-						<input
-							type="password"
-							name="password"
-							value={Password}
-							onChange={this.onChangePassword}
-							required
-						/>
-					</div>
-					<br />
-					<div>
-						<button type="submit" value="Submit">
-							Submit
-						</button>
-						<br />
-						<hr />
-					</div>
-				</form>
-			</React.Fragment>
-		);
-	}
+  onChangePassword(event) {
+    this.setState({
+      Password: event.target.value
+    });
+  }
+  onSubmit = async event => {
+    event.preventDefault();
+    try {
+      await (this.state.Username, this.state.Password);
+      alert("login successfull!");
+      this.props.history.push("/login");
+    } catch (event) {
+      alert(event.message);
+    }
+    const { Username, Password } = this.state;
+    const apiUrl = "http://localhost:3001/users/login";
+    return axios
+      .post(apiUrl, {
+        Username,
+        Password
+      })
+      .then(res => {
+        window.localStorage.setItem("jwt", res.token);
+        if (res.token === window.localStorage.token) {
+          this.props.history.push("/profile");
+        }
+      });
+  };
+  onChange(key, event) {
+    this.setState({
+      [key]: event.target.value
+    });
+  }
+  render() {
+    const { Username, Password } = this.state;
+    return (
+      <React.Fragment>
+        <form id="login" name="login" method="POST" onSubmit={this.onSubmit}>
+          <div>
+            <h2>Please Login</h2>
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={Username}
+              onChange={this.onChangeUsername}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={Password}
+              onChange={this.onChangePassword}
+              required
+            />
+          </div>
+          <br />
+          <div>
+            <button type="submit" value="Submit">
+              Submit
+            </button>
+            <br />
+            <hr />
+          </div>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Login;
--- react-frontend-inventory/src/components/Profile.js
+++ react-frontend-inventory/src/components/Profile.js
@@ -1,138 +1,130 @@
-<<<<<<< Updated upstream
-import React, { Component } from 'react';
-import axios from 'axios';
-//import React from 'react';
-import '../App.css';
-=======
-//import React, {Component} from 'react';
-import React from "react";
+import React, { Component } from "react";
+// import React from "react";
 import "../App.css";
->>>>>>> Stashed changes
+import axios from "axios";
 
-/*class Profile extends Component {
-	constructor(props) {
-		super(props);
-		 this.state = {
-			 profile: true,
-			result: [],
-		};
-    }
-componentDidMount() {
-  fetch("http://localhost:3001/users/profile")
-  .then(response => response.json())
-  .then( responseJson=> {
-    this.setState({ results:responseJson.data });
-  },
-  )}
-render(){
-    return(
-<div>
-<h1>Your Employee Information:</h1>
-<ul>
-<li>
-	{result.FirstName}{result.LastName}
-</li>
-<li>
-	{result.Email}
-</li>
-<li>
-	{result.Username}
-</li>
-<hr />
-<li>
-	{result.EmployeeNumber}
-</li>
-</ul>
-</div>
-)
-};
-*/
+// class Profile extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       profile: true,
+//       result: []
+//     };
+//   }
+//   componentDidMount() {
+//     fetch("http://localhost:3001/users/profile")
+//       .then(response => response.json())
+//       .then(responseJson => {
+//         this.setState({ results: responseJson.data });
+//       });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Your Employee Information:</h1>
+//         <ul>
+//           <li>
+//             {result.FirstName}
+//             {result.LastName}
+//           </li>
+//           <li>{result.Email}</li>
+//           <li>{result.Username}</li>
+//           <hr />
+//           <li>{result.EmployeeNumber}</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
-<<<<<<< Updated upstream
+// const Profile = () => (
+//   <div>
+//     <div>
+//       <img src={require("../images/avatar.png")} height="45px" alt="Avatar" />
+//       <p>Your Employee Information:</p>
+//       <hr />
+//     </div>
+
+//     <div>
+//       <img src={require("../images/avatar.png")} height="45px" alt="Avatar" />
+//       <h1>Your Employee Information:</h1>
+//       <hr />
+//     </div>
+//   </div>
+// );
+
 class Profile extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			users: [],
-			isLoading: true,
-			errors: null
-		};
-	}
+    this.state = {
+      users: [],
+      isLoading: true,
+      errors: null
+    };
+  }
 
-	componentDidMount() {
-		axios
-			.get('http://localhost:3001/users/profile')
-			//.then(() => {
-			//	localStorage.getItem('jwt');
-			//})
-			.then((response) =>
-				response.data.results.map((user) => ({
-					FirstName: `${user.FirstName}`,
-					LastName: `${user.LastName}`,
-					Username: `${user.Username}`,
-					Email: `${user.Email}`,
-					UserId: `${user.UserId}`
-				}))
-			)
-			.then((users) => {
-				this.setState({
-					users,
-					isLoading: false
-				});
-			})
-			.catch((error) => this.setState({ error, isLoading: false }));
-	}
-=======
-const Profile = () => (
-<<<<<<< Updated upstream
-	<div>
-		<img src={require('../images/avatar.png')} height="45px" alt="Avatar" />
-		<p>Your Employee Information:</p>
-		<hr />
-	</div>
-=======
-  <div>
-    <img src={require("../images/avatar.png")} height="45px" alt="Avatar" />
-    <h1>Your Employee Information:</h1>
-    <hr />
-  </div>
->>>>>>> Stashed changes
-);
->>>>>>> Stashed changes
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/users/profile")
+      //.then(() => {
+      //	localStorage.getItem('jwt');
+      //})
+      .then(response =>
+        response.data.results.map(user => ({
+          FirstName: `${user.FirstName}`,
+          LastName: `${user.LastName}`,
+          Username: `${user.Username}`,
+          Email: `${user.Email}`,
+          UserId: `${user.UserId}`
+        }))
+      )
+      .then(users => {
+        this.setState({
+          users,
+          isLoading: false
+        });
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
 
-	render() {
-		const { isLoading, users } = this.state;
-		return (
-			<React.Fragment>
-				<div>
-					{!isLoading ? (
-						users.map((user) => {
-							const { Username, LastName, Email, FirstName, UserId } = user;
-							return (
-								<div key={UserId}>
-									<p>
-										{FirstName} {LastName}
-									</p>
-									<div>
-										<p>{Email}</p>
-									</div>
-									<p>{Username}</p>
-									<hr />
-								</div>
-							);
-						})
-					) : (
-						<p>Loading...</p>
-					)}
-				</div>
-				<div>
-					<img src={require('../images/avatar.png')} height="45px" alt="Avatar" />
-					<p>Your Employee Information:</p>
-					<div />
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    const { isLoading, users } = this.state;
+    return (
+      <React.Fragment>
+        <div>
+          {!isLoading ? (
+            users.map(user => {
+              const { Username, LastName, Email, FirstName, UserId } = user;
+              return (
+                <div key={UserId}>
+                  <p>
+                    {FirstName} {LastName}
+                  </p>
+                  <div>
+                    <p>{Email}</p>
+                  </div>
+                  <p>{Username}</p>
+                  <hr />
+                </div>
+              );
+            })
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+        <div>
+          <img
+            src={require("../images/avatar.png")}
+            height="45px"
+            alt="Avatar"
+          />
+          <p>Your Employee Information:</p>
+          <div />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
+
 export default Profile;
